d8fba0a42b2284a4b6607cdeb2ce1ac472ec14ef
pmojicap <56459275+pmojicap@users.noreply.github.com>
2019-10-18 17:25:53 -0500

    Update SolucionS2.r

--- Semana 2/SolucionS2.r
+++ Semana 2/SolucionS2.r
@@ -1,6 +1,6 @@
 #EJERCICIOS SEMANA 3#
 
-#Antes que nada se deben installar y cargar las librerias necesairas para la practica#
+#Antes que nada se deben instalar y cargar las librerias necesairas para la practica#
 
 library(ape)
 library(permute)
@@ -20,22 +20,22 @@ data(corvus)
 data(tortues)
 data(geospiza)
 
-#¿Que clase de objeto es BIC? ¿Qué clase de objeto es geospiza? use la función 
-#*str* para obtener información sobre la estructura.
+#Â¿Que clase de objeto es BIC? Â¿QuÃ© clase de objeto es geospiza? use la funciÃ³n 
+#*str* para obtener informaciÃ³n sobre la estructura.
 
 class(BCI)
 class(geospiza)
 str(BCI)
 str(geospiza)
 
-#¿Qué es geospiza?(informacion sobre los datos: autor,significado de las variables etc.)
+#Â¿QuÃ© es geospiza?(informacion sobre los datos: autor,significado de las variables etc.)
 
 help(geospiza)
 ?geospiza
 
-#¿Qué tipo de variable es tarsusL y qué clase? 
-#Abra el objeto *tortues* y mire ¿Qué tipo de variables tiene y qué clase son?, haga lo mismo con el objeto *mite.env* y revise 
-#¿Qué tipo de variable es *shurb* y qué clase es?
+#Â¿QuÃ© tipo de variable es tarsusL y quÃ© clase? 
+#Abra el objeto *tortues* y mire Â¿QuÃ© tipo de variables tiene y quÃ© clase son?, haga lo mismo con el objeto *mite.env* y revise 
+#Â¿QuÃ© tipo de variable es *shurb* y quÃ© clase es?
 
 class(geospiza$tarsusL)
 
@@ -54,7 +54,7 @@ class(mite.env$Substrate)
 class(mite.env$Shrub)
 class(mite.env$Topo)
 
-#Usando el data.frame *tortues* calcule la proporción de hembras y machos de la muestra
+#Usando el data.frame *tortues* calcule la proporciÃ³n de hembras y machos de la muestra
 #proporcion de machos
 sexos <- c('F', 'M')
 M <- length(which(tortues == 'M'))
@@ -64,7 +64,7 @@ F
 paste(F,'/',M,sep = '')
 F/M
 
-#Usando el data.frame *tortues* calcule la proporción entre el ancho y el largo del caparazón, genere una tabla con la información de *tortues* 
+#Usando el data.frame *tortues* calcule la proporciÃ³n entre el ancho y el largo del caparazÃ³n, genere una tabla con la informaciÃ³n de *tortues* 
 #y la nueva variable derivada.
 medidas <-cbind(tortues$long,tortues$larg)
 colnames(medidas)<-c("Largo","Ancho")
@@ -72,8 +72,8 @@ proporcion <- tortues$long/tortues$larg
 relacion <-cbind(medidas,proporcion)
 relacion
 
-#¿Cuántas especies de *Corvus* fueron muestreadas en la región Paleártica-Africana? Use el data.frame del género 
-#*Corvus*. ¿Qué porcentaje de *Corvus* muestreados habitan zonas abiertas
+#Â¿CuÃ¡ntas especies de *Corvus* fueron muestreadas en la regiÃ³n PaleÃ¡rtica-Africana? Use el data.frame del gÃ©nero 
+#*Corvus*. Â¿QuÃ© porcentaje de *Corvus* muestreados habitan zonas abiertas
 no.muestreos <- table(corvus$phylog)
 no.muestreos
 
@@ -85,18 +85,18 @@ prop.table(zonas)
 
 #0.61%
 
-#¿Cómo luce el gráfico de frecuencias para los tipos de hábitat del género *Corvus*?
+#Â¿CÃ³mo luce el grÃ¡fico de frecuencias para los tipos de hÃ¡bitat del gÃ©nero *Corvus*?
 plot(prop.table(zonas), main= "Corvus por habitat", ylab ="Frecuencia", col=c("green","orange"))
 
-#Encuentre el máximo, así como lo hizo para el mínimo
+#Encuentre el mÃ¡ximo, asÃ­ como lo hizo para el mÃ­nimo
 atlas$birds
 sp <- apply(atlas$birds,2,sum)
 sp
 max(sp)
 min(sp)
 
-#Repita el ejercicio para las variables continuas de tamaño de pico y tamaño del ala en el género *Corvus*. 
-#Busque cómo cambiar colores, ejes y algunas propiedades que le permitan mejorar el diseño del histograma.  
+#Repita el ejercicio para las variables continuas de tamaÃ±o de pico y tamaÃ±o del ala en el gÃ©nero *Corvus*. 
+#Busque cÃ³mo cambiar colores, ejes y algunas propiedades que le permitan mejorar el diseÃ±o del histograma.  
 
 max(corvus$bill)
 min(corvus$bill)
@@ -106,8 +106,8 @@ max(corvus$wing)
 min(corvus$wing)
 range(corvus$wing)
 
-hist(corvus$bill, col= rainbow(8), main = "Histograma picos de Corvus" ,xlab = "Tamaño (mm)", seq(40,90,10))
-hist(corvus$wing, col= rainbow(6), main = "Histograma alas de Corvus" ,xlab = "Tamaño (mm)")
+hist(corvus$bill, col= rainbow(8), main = "Histograma picos de Corvus" ,xlab = "TamaÃ±o (mm)", seq(40,90,10))
+hist(corvus$wing, col= rainbow(6), main = "Histograma alas de Corvus" ,xlab = "TamaÃ±o (mm)")
 
 #Invente unos datos de medidas, como se hizo en el caso de los craneos, y genere todo el ejercicio hasta el histograma 
 ancho <- rnorm(n = 25, mean = 15, sd = 1.5)
@@ -116,4 +116,4 @@ area <- largo*ancho
 area
 cuadros <- cbind.data.frame(ancho, largo, area)
 cuadros
-hist(cuadros$area, main = "Área de los cuadros", xlab = "Área", col= terrain.colors(7))
+hist(cuadros$area, main = "Ãrea de los cuadros", xlab = "Ãrea", col= terrain.colors(7))
